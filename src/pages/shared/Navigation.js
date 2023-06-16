f69ffe396e03f6d6a560827e7811b52087fd9288
soko <saramsa2@gmail.com>
2023-06-16 21:04:19 +1200

    initial commit

---
+++ src/pages/shared/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = ({children}) => {
+  return (
+    <div>
+      <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+        <div className='container-fluid'>
+          <span>Princess Natural Beding</span>
+        </div>
+
+      </nav>
+      
+    </div>
+  );
+};
+
+export default Navigation;
